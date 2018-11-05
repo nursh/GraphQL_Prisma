3b390a5560b9ca7bc4a9895d836fab12b1c623b2
nursh <nsheikhmohammed@yahoo.com>
2018-11-05 17:25:42 -0500

    Added fragments and refactored resolvers

--- src/index.js
+++ src/index.js
@@ -1,34 +1,14 @@
 import { GraphQLServer, PubSub } from "graphql-yoga";
 import db from "./db";
-import Query from "./resolvers/Query";
-import Mutation from "./resolvers/Mutation";
-import Subscription from "./resolvers/Subscription";
-import User from "./resolvers/User";
-import Post from "./resolvers/Post";
-import Comment from "./resolvers/Comment";
+import { resolver } from "./resolvers/index";
 
 import prisma from "./prisma";
-
-// Resolvers
-
-// 4 args passed into resolvers
-// parent -> for relational data
-// args -> data being passed into the query
-// context | ctx -> for context of the data
-// info
 
 const pubsub = new PubSub();
 
 const server = new GraphQLServer({
   typeDefs: "./src/schema.graphql",
-  resolvers: {
-    Query,
-    Mutation,
-    Subscription,
-    User,
-    Post,
-    Comment
-  },
+  resolvers,
   context(request) {
     return {
       db,
