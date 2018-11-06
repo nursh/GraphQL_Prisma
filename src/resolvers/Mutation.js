06fc227a4ce49fb9764d5d10d1ee788f12ec917b
nursh <nsheikhmohammed@yahoo.com>
2018-11-05 19:43:02 -0500

    Refactored hashed passowrd

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,6 +1,7 @@
 import bcrypt from "bcryptjs";
 import generateToken from "../utils/generateToken";
 import getUserId from "../utils/getUserId";
+import hashPassword from '../utils/hashPassword';
 
 const Mutation = {
   async login(parent, args, { prisma }, info) {
@@ -22,13 +23,11 @@ const Mutation = {
     };
   },
   async createUser(parent, args, { prisma }, info) {
-    if (args.data.password.length < 8)
-      throw new Error("Password must be at least 8 characters");
 
     const emailTaken = await prisma.exists.User({ email: args.data.email });
     if (emailTaken) throw new Error("Email taken");
 
-    const password = await bcrypt.hash(args.data.password, 10);
+    const password = await hashPassword(args.data.password);
     const user = await prisma.mutation.createUser({
       data: {
         ...args.data,
@@ -43,6 +42,11 @@ const Mutation = {
   async updateUser(parent, args, { prisma, request }, info) {
     // You don't need the error checking, it's just to provide a better error message
     const userId = getUserId(request);
+
+    if (typeof args.data.password === 'string') {
+      args.data.password = await hashPassword(args.data.password);
+    }
+
     const opArgs = {
       where: {
         id: userId
