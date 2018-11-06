aa55e9a1a1c97abbf0c5991b21a227e13a1b8c45
nursh <nsheikhmohammed@yahoo.com>
2018-11-05 19:01:18 -0500

    Added authentication to myPost subscription

--- src/resolvers/Subscription.js
+++ src/resolvers/Subscription.js
@@ -1,3 +1,5 @@
+import getUserId from "../utils/getUserId";
+
 const Subcription = {
   comment: {
     subscribe(parent, { postId }, { prisma }, info) {
@@ -23,6 +25,23 @@ const Subcription = {
         }
       };
       return prisma.subscription.post(opArgs, info);
+    }
+  },
+  myPost: {
+    subscribe(parent, args, { prisma, request }, info) {
+      const userId = getUserId(request);
+      return prisma.subscription.post(
+        {
+          where: {
+            node: {
+              author: {
+                id: userId
+              }
+            }
+          }
+        },
+        info
+      );
     }
   }
 };
